749a28f32bf7df93a07b567f32a67d07e062077e
Felipe Mendes Neves <felipemen74@gmail.com>
2024-12-26 20:55:49 -0300

    remove route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
